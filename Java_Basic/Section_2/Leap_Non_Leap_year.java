c821b6f774550e30d9b4f62407defe81c0d54dd4
Visagan126 <visagan126@gmail.com>
2025-12-17 16:30:49 +0530

    Java Files added

---
+++ Java_Basic/Section_2/Leap_Non_Leap_year.java
@@ -0,0 +1,13 @@
+package Java_Basic.Section_2;
+import java.util.*;
+public class Leap_Non_Leap_year {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the year: ");
+		int year=s.nextInt();
+		
+
+	}
+
+}
